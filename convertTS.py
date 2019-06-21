84a69f62757690d0587b8203ff9b71923b4aa7ce
jgurnett <gurnettt@mymacewan.ca>
2019-06-21 15:01:33 -0600

    added comments

--- convertTS.py
+++ convertTS.py
@@ -1,44 +1,62 @@
+#  Joel Gurnett
+#  Converts ts files to mp4
+#  June 21, 2019
+
 #!/usr/bin/python
 import subprocess
 import os
 from ffmpy import FFmpeg
 
+# gets all ts files that are in given path
+#
+# dirPath = string of path given
 def getFiles(dirPath):
 	videoFiles = []
 	files = os.listdir(dirPath)
+	# go through all files in directory
 	for index, file in enumerate(files):
 		filename, file_extension = os.path.splitext(file)
-		# print("file " + str(index) +" : " + filename)
+
+		# if the file is a directory then iterate through that
 		if(os.path.isdir(dirPath + file)):
+			# don't look at hidden files
 			if(file[0] != "."):
 				inner = os.listdir(dirPath + file)
 				for tmp in inner:
 					tmpname, tmp_extension = os.path.splitext(tmp)
 					if tmp_extension == ".ts":
 						videoFiles.append(dirPath + file + "/" + tmp)
+
+
 		if file_extension == ".ts":
 			videoFiles.append(dirPath + file)
 
 	if len(videoFiles) > 0:
 		return videoFiles
-
 	return None
 
 
+# execute program
 def main():
+	# main path to start on 
 	dirPath = "/Users/joelgurnett/Downloads/show/"
+	# get list of ts files
 	vids = getFiles(dirPath)
+
+	# iterate through the list
 	if vids != None:
 		for index, video in enumerate(vids):
 			filename, file_extension = os.path.splitext(video)
 			infile = video
 			outfile = filename + ".mp4"
-			print("file: " + infile)
-			print("outfile: " + outfile)
-			for file in vids:
-				print(file)
 
+			# display all ts files
+			# for file in vids:
+			# 	print(file)
+
+			# convert ts to mp4
 			p = subprocess.call(["ffmpeg", "-i", infile, outfile])
+			# rmove ts file
 			os.remove(infile)
 	else:
 		print("no files")
