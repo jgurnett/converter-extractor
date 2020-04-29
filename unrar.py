303fa389739c4d2ad4c1ecb4cfba75a076d7a1a5
jgurnett <gurnettt@mymacewan.ca>
2020-04-29 10:45:18 -0600

    made some comments

--- unrar.py
+++ unrar.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python
 #  Joel Gurnett
-#  Converts ts files to mp4
-#  June 21, 2019
+#  unrars files, checks if they are already part of an extracted file
+#  April 29, 2020
 
 from pyunpack import Archive
 import globalFunc
+
+# create a list of files that have been extracted in the past
 def convertedList(path):
 	# define an empty list
 	places = []
@@ -24,14 +26,17 @@ def convertedList(path):
 
 # execute program
 def main():
+	# path to converted files list
 	convPath = "/Users/joelgurnett/Desktop/convertedFiles.txt"
 	# main path to start on 
 	dirPath = "/Users/joelgurnett/Desktop/another/"
+	# save the file to a list
 	convList = convertedList(convPath)
 	# get list of ts files
 	files = globalFunc.getFiles(dirPath)
 	# iterate through the list
 	if files != None:
+		# if the file already exists append to it, else create a new one
 		if (convList != None):
 			f = open(convPath, "a+")
 		else:
@@ -40,11 +45,12 @@ def main():
 			if (rar not in convList):
 				filename, file_extension = os.path.splitext(rar)
 				infile = rar
+				
+				# extract the file to this folder
 				Archive(rar).extractall("/Users/joelgurnett/Desktop/another/")
+				# write it to the file with the extracted files
 				f.write("\n" + filename + file_extension)
 
 		f.close()
-	else:
-		print("no files")
 
 main()
