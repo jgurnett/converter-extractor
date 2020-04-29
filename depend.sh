d77852d7abba1facbe926e0d1fe2cb65f0f660bd
jgurnett <gurnettt@mymacewan.ca>
2020-04-29 15:36:50 -0600

    Update depend.sh

--- depend.sh
+++ depend.sh
@@ -2,7 +2,8 @@
 
 chmod +x convertTS.py
 sudo apt --assume-yes install ffmpeg
-sudo apt --assume-yes install python-pip3
+sudo apt --assume-yes install python3-pip
+sudo apt --assume-yes install unrar
 sudo pip3 install ffmpy
 sudo pip3 install pyunpack
 sudo pip3 install patool
