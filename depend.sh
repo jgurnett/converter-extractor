28a5baad4b81ff911ac16a7ba7884c7e8913f19d
jgurnett <gurnettt@mymacewan.ca>
2020-04-29 10:34:06 -0600

    added dependancies for unrar

--- depend.sh
+++ depend.sh
@@ -2,5 +2,7 @@
 
 chmod +x convertTS.py
 sudo apt --assume-yes install ffmpeg
-sudo apt --assume-yes install python-pip
-sudo pip install ffmpy
+sudo apt --assume-yes install python-pip3
+sudo pip3 install ffmpy
+sudo pip3 install pyunpack
+sudo pip3 install patool
